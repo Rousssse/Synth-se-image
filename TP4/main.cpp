dd892b14e14709eff19a9c50a73e7ba99e95282d
Elisa <elisakarateka@gmail.com>
2023-02-28 15:45:26 +0100

    sphere

--- TP4/main.cpp
+++ TP4/main.cpp
@@ -1,9 +1,17 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <glimac/Program.hpp>
+#include <glimac/FilePath.hpp>
+#include <glimac/glm.hpp>
+#include <glimac/common.hpp>
+#include <glimac/Sphere.hpp>
+#include <glimac/getTime.hpp>
+#include <cstddef>
 
 int window_width  = 1280;
 int window_height = 720;
+//float ratio = window_width/window_height;
 
 static void key_callback(GLFWwindow* /*window*/, int /*key*/, int /*scancode*/, int /*action*/, int /*mods*/)
 {
@@ -27,12 +35,25 @@ static void size_callback(GLFWwindow* /*window*/, int width, int height)
     window_height = height;
 }
 
-int main()
+struct Vertex2DColor {
+
+    glm::vec2 position;
+    glm::vec3 color;
+
+    Vertex2DColor(){};
+    Vertex2DColor(const glm::vec2 pos, const glm::vec3 col):position(pos),color(col){};
+
+};
+
+
+int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[])
 {
     /* Initialize the library */
     if (!glfwInit()) {
         return -1;
     }
+
+
 
     /* Create a window and its OpenGL context */
 #ifdef __APPLE__
@@ -42,7 +63,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
-    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "TP4", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "TP1", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -57,16 +78,69 @@ int main()
     }
 
     /* Hook input callbacks */
+    
+
     glfwSetKeyCallback(window, &key_callback);
     glfwSetMouseButtonCallback(window, &mouse_button_callback);
     glfwSetScrollCallback(window, &scroll_callback);
     glfwSetCursorPosCallback(window, &cursor_position_callback);
     glfwSetWindowSizeCallback(window, &size_callback);
 
+    glimac::Sphere sphere(1, 32, 16);
+
+    GLuint vbo;
+    glGenBuffers(1,&vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sphere.getVertexCount()*sizeof(glimac::ShapeVertex), sphere.getDataPointer(), GL_STATIC_DRAW);
+    glBindBuffer(0,vbo);
+
+    GLuint vao;
+    glGenVertexArrays(1,&vao);
+    glBindVertexArray(vao);
+    //const GLuint VERTEX_ATTR_POSITION = 0;
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex,position)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex, normal)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)(offsetof(glimac::ShapeVertex, texCoords)));
+    glBindBuffer(0,vbo);
+    glBindVertexArray(0);
+
+    glimac::FilePath applicationPath ="/home/elisa/OPENGLMAX/GLImac-Template/TP4/shaders/";
+    std::cout << applicationPath << std::endl;
+    glimac::Program program = loadProgram(applicationPath.dirPath() + "shaders/3D.vs.glsl", applicationPath.dirPath() + "shaders/normals.fs.glsl");
+    program.use();
+
+    GLint uMVPMatrix = glGetUniformLocation(program.getGLId(), "uMVPMatrix");
+    GLint uMVMatrix = glGetUniformLocation(program.getGLId(), "uMVMatrix");
+    GLint uNormalMatrix = glGetUniformLocation(program.getGLId(), "uNormalMatrix");
+
+    glEnable(GL_DEPTH_TEST);
+    glm::mat4 ProjMatrix = glm::perspective(glm::radians(70.f), (GLfloat)window_width/(GLfloat)window_height, 0.1f, 100.f);
+    glm::mat4 MVMatrix = glm::translate(glm::mat4(1.0f),glm::vec3(0.0f,0.0f,-5.0f));
+    glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+    
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.f, 0.75f, 0.75f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1.f, 0.5f, 0.5f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        glBindVertexArray(vao);
+        glUniformMatrix4fv(uMVPMatrix,1, GL_FALSE, glm::value_ptr(ProjMatrix*MVMatrix));
+        glUniformMatrix4fv(uMVMatrix,1, GL_FALSE, glm::value_ptr(MVMatrix));
+        glUniformMatrix4fv(uNormalMatrix,1, GL_FALSE, glm::value_ptr(NormalMatrix));
+
+        glDrawArrays(GL_TRIANGLES, 0, sphere.getVertexCount());
+
+        glm
+
+        glDrawArrays()
+
+        glBindVertexArray(0);
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -75,5 +149,7 @@ int main()
     }
 
     glfwTerminate();
+    glDeleteBuffers(0,&vbo);
+    glDeleteVertexArrays(0,&vao);
     return 0;
 }
